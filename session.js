c039156bec04e4b863206209cf991b2fcfbb6baf
monarch21 <96679851+monarch21@users.noreply.github.com>
2022-01-27 11:01:09 +0530

    Update session.js

--- session.js
+++ session.js
@@ -1,7 +1,1 @@
-{
-        "clientID": "ixYntEfAhGxrhQcAi921+w==",
-        "serverToken": "1@8xYuPIOkPsO2kw8uoSchEmvX2mrHYDxTKr3O/kelME8QkHZp4rRU/HcU>",   
-        "clientToken": "QWwM1pgq1+BtG6n7/8BLWNVhMb/Cv1/oisFxt1w0XAk=",
-        "encKey": "0qC3vP+IJdvVPx52dTgwmeU8MOBYrQLe7ZleGg+bFFk=",
-        "macKey": "ZbNM499er9kGh1ifm9L1/lLphOWa7NunQ0o9Zu3jK6A="
-}
+{"clientID: "ixYntEfAhGxrhQcAi921+w==","serverToken:"1@8xYuPIOkPsO2kw8uoSchEmvX2mrHYDxTKr3O/kelME8QkHZp4rRU/HcU>","clientToken:"QWwM1pgq1+BtG6n7/8BLWNVhMb/Cv1/oisFxt1w0XA=","encKey: "0qC3vP+IJdvVPx52dTgwmeU8MOBYrQLe7ZleGg+bFFk=","macKey: "ZbNM499er9kGh1ifm9L1/lLphOWa7NunQ0o9Zu3jK6A="}
