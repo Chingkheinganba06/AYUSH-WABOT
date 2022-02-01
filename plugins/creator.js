3320884f1136f9f3756d79f1b36e2bd0a9acc379
monarch21 <96679851+monarch21@users.noreply.github.com>
2022-02-01 23:25:33 +0530

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '918130784851','919574584820''kazukafu', m)
+  this.sendContact(m.chat, '918130784851','AYUSH', m)
 }
 handler.help = ['owner/creator/mod']
 handler.tags = ['info']
