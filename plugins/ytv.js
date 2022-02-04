58741f497d902a389a0b8f945a59c5f10c63133d
monarch21 <96679851+monarch21@users.noreply.github.com>
2022-02-04 09:48:39 +0530

    Update ytv.js

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -11,6 +11,8 @@ let res = await post(`https://www.y2mate.com/mates/en68/analyze/ajax`, {
 const mela = await res.json()
 const $ = cheerio.load(mela.result)
 const hasil = []
+let handler = async (m, { conn, args, isPrems, isOwner }) => {
+if (!args || !args[0]) throw 'Uhm... where is the url?'
 let thumb = $('div').find('.thumbnail.cover > a > img').attr('src')
 let judul = $('div').find('.thumbnail.cover > div > b').text()
 let quality = $('div').find('#mp4 > table > tbody > tr:nth-child(4) > td:nth-child(3) > a').attr('data-fquality')
