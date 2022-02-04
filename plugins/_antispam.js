564b351d122f9be79d09a8c115dafc48c0395095
monarch21 <96679851+monarch21@users.noreply.github.com>
2022-02-04 10:41:34 +0530

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -8,17 +8,15 @@ handler.all = async function (m) {
         if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
             if (this.spam[m.sender].count > 10) {
                 //global.db.data.users[m.sender].banned = true
-                m.reply('*Do not Spam!! Otherwise you will kicked*')
-        
-                if (isowner) return m.reply('*Hey sorry you\'re owner , you won\'t be kicked. haha..*')
-            } 
-            this.spam[m.sender].count = 10
+                m.reply('*Do not Spam!!*')
+            }
+            this.spam[m.sender].count = 0
             this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
         }
     }
     else this.spam[m.sender] = {
         jid: m.sender,
-        count: 10,
+        count: 0,
         lastspam: 0
     }
 }
